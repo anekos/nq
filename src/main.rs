964f79028c6c057d9b391a631f338d000dddf92d
anekos <anekos@snca.net>
2017-12-13 21:44:38 +0900

    Improve

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@
 #[macro_use] extern crate serde_derive;
 extern crate docopt;
 extern crate encoding;
-extern crate mktemp;
 extern crate quick_csv;
 extern crate rusqlite;
 
 
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File, metadata};
 use std::io::Read;
-use std::process::{exit, Command};
 use std::os::unix::process::CommandExt;
+use std::path::Path;
+use std::process::{exit, Command};
 
 use docopt::Docopt;
-use mktemp::Temp;
+use encoding::DecoderTrap;
+use encoding::label::encoding_from_whatwg_label;
 use quick_csv::Csv;
 use rusqlite::types::ToSql;
 use rusqlite::{Connection, Transaction};
@@ -25,11 +26,12 @@ const USAGE: &'static str = "
 not q
 
 Usage:
-  nq [options] <csv> [--] <sqlite-options>...
+  nq [options] <csv> [-- <sqlite-options>...]
   nq (-h | --help)
   nq --version
 
 Options:
+  -e ENCODING   CSV character encoding: https://encoding.spec.whatwg.org/#concept-encoding-get
   -q SQL        SQL
   -c CACHE      Cache *.sqlite
   -h --help     Show this screen.
@@ -40,8 +42,9 @@ Options:
 #[derive(Debug, Deserialize)]
 struct AppOptions {
     arg_csv: String,
-    flag_sqlite_cache: Option<String>,
+    flag_c: Option<String>,
     flag_q: Option<String>,
+    flag_e: Option<String>,
     arg_sqlite_options: Vec<String>,
 }
 
@@ -56,46 +59,67 @@ fn main() {
 
 fn nq() -> Result<(), Box<Error>> {
     let options: AppOptions = Docopt::new(USAGE).and_then(|d| d.deserialize()).unwrap_or_else(|e| e.exit());
-    println!("args: {:?}", options);
 
-    let sqlite_filepath = make_sqlite(&options.flag_sqlite_cache)?;
+    let cache_filepath = make_sqlite(&options.arg_csv, &options.flag_c)?;
+    let cache_is_fresh = is_fresh(&options.arg_csv, &cache_filepath)?;
 
-    let mut buffer = String::new();
-    let mut csv = open_csv(&options.arg_csv, &mut buffer)?;
+    if !cache_is_fresh {
+        let mut buffer = String::new();
+        let mut csv = open_csv(&options.arg_csv, &mut buffer, &options.flag_e)?;
 
-    let header = csv.next().ok_or("Header not found")??;
-    let header = header.columns()?.collect::<Vec<&str>>();
+        let header = csv.next().ok_or("Header not found")??;
+        let header = header.columns()?.collect::<Vec<&str>>();
 
-    let mut conn = Connection::open(&sqlite_filepath)?;
-    let tx = conn.transaction()?;
+        let mut conn = Connection::open(&cache_filepath)?;
+        let tx = conn.transaction()?;
 
-    create_table(&tx, header.as_slice())?;
-    insert_rows(&tx, header.len(), csv)?;
+        create_table(&tx, header.as_slice())?;
+        insert_rows(&tx, header.len(), csv)?;
 
-    tx.commit()?;
+        tx.commit()?;
+    }
 
-    exec_sqlite(&sqlite_filepath, &options.flag_q, &options.arg_sqlite_options);
+    exec_sqlite(&cache_filepath, &options.flag_q, &options.arg_sqlite_options);
 
     Ok(())
 }
 
 
-fn make_sqlite(cache_filepath: &Option<String>) -> Result <String, Box<Error>> {
+fn make_sqlite(sqlite_filepath: &str, cache_filepath: &Option<String>) -> Result <String, Box<Error>> {
     match *cache_filepath {
         Some(ref path) => Ok(path.clone()),
         None => {
-            let temp = Temp::new_file()?;
-            let path = temp.to_path_buf();
-            let path = path.to_str().ok_or("Invalid path")?;
-            Ok(path.to_owned())
+            let mut path = sqlite_filepath.to_owned();
+            path.push_str(".nq-cache.sqlite");
+            Ok(path)
         }
     }
 }
 
-fn open_csv<'a>(csv_filepath: &str, buffer: &'a mut String) -> Result<Csv<&'a [u8]>, Box<Error>> {
+fn open_csv<'a>(csv_filepath: &str, buffer: &'a mut String, encoding: &Option<String>) -> Result<Csv<&'a [u8]>, Box<Error>> {
     let mut file = File::open(csv_filepath)?;
-    file.read_to_string(buffer)?;
+
+    if let Some(ref encoding) = *encoding {
+        let encoding = encoding_from_whatwg_label(encoding).ok_or("Invalid encoding name")?;
+        let mut bin: Vec<u8> = vec![];
+        file.read_to_end(&mut bin)?;
+        *buffer = match encoding.decode(&bin, DecoderTrap::Replace) {
+            Ok(s) => s,
+            Err(s) => s.to_string(),
+        };
+    } else {
+        file.read_to_string(buffer)?;
+    }
     Ok(quick_csv::Csv::from_string(buffer))
+}
+
+fn is_fresh(csv_filepath: &str, sqlite_filepath: &str) -> Result<bool, Box<Error>> {
+    if !Path::new(sqlite_filepath).exists() {
+        return Ok(false)
+    }
+    let csv = metadata(csv_filepath)?.modified()?;
+    let sqlite = metadata(sqlite_filepath)?.modified()?;
+    Ok(csv < sqlite)
 }
 
 fn create_table(tx: &Transaction, header: &[&str]) -> Result<(), Box<Error>> {
@@ -139,7 +163,7 @@ fn insert_rows(tx: &Transaction, headers: usize, rows: Csv<&[u8]>) -> Result<(),
     for row in rows {
         n += 1;
         if n % 100 == 0 {
-            println!("{} rows", n);
+            eprintln!("{} rows", n);
         }
         if let Ok(row) = row {
             let row: Vec<&str> = row.columns()?.collect();
@@ -148,7 +172,7 @@ fn insert_rows(tx: &Transaction, headers: usize, rows: Csv<&[u8]>) -> Result<(),
         }
     }
     if n % 100 != 0 {
-        println!("{} rows", n);
+        eprintln!("{} rows", n);
     }
 
     Ok(())
